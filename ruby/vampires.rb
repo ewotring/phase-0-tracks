ef9e85b56187dcd2310d0206df339e5a6caf2842
Erik Wotring <ewotring@gmail.com>
2017-05-10 17:12:09 -0500

    Add questions and data collection for Release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+new_name=gets.chomp
+puts "How old are you?"
+age=gets.chomp
+puts "What year were your born?"
+year=gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+bread=gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance=gets.chomp
