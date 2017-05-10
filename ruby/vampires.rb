57dd5b4ab488f925e5cd2deb5a072243c9df1fc0
Erik Wotring <ewotring@gmail.com>
2017-05-10 17:49:44 -0500

    Add detection logic for Release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,34 @@
 puts "What is your name?"
 new_name=gets.chomp
 puts "How old are you?"
-age=gets.chomp
+age=gets.chomp.to_i
 puts "What year were your born?"
-year=gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+year=gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 bread=gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (y/n)"
 insurance=gets.chomp
+
+result="Results inconclusive."
+
+if 2017-year==age && (bread=="y" || insurance=="y")
+    result="Probably not a vampire."
+end
+
+if 2017-year!=age && (bread=="n" || insurance=="n")
+    result="Probably a vampire."
+end
+
+if 2017-year!=age && bread=="n" && insurance=="n"
+    result="Probably a vampire."
+end
+
+if 2017-year!=age && bread=="n" && insurance=="n"
+    result="Almost certainly a vampire."
+end
+
+if new_name=="Drake Cula" || new_name=="Tu Fang"
+    result="Definitely a vampire."
+end
+
+puts "#{result}"
