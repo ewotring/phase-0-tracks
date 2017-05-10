d00e5ded0ea53b8176651e47f7e23a97054a639a
Erik Wotring <ewotring@gmail.com>
2017-05-10 18:02:33 -0500

    Add ability to process multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,34 +1,41 @@
-puts "What is your name?"
-new_name=gets.chomp
-puts "How old are you?"
-age=gets.chomp.to_i
-puts "What year were your born?"
-year=gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-bread=gets.chomp
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-insurance=gets.chomp
+puts "How many employees will be processed?"
+employees=gets.chomp.to_i
+i=0
 
-result="Results inconclusive."
+while i < employees do
+    puts "What is your name?"
+    new_name=gets.chomp
+    puts "How old are you?"
+    age=gets.chomp.to_i
+    puts "What year were your born?"
+    year=gets.chomp.to_i
+    puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+    bread=gets.chomp
+    puts "Would you like to enroll in the company's health insurance? (y/n)"
+    insurance=gets.chomp
 
-if 2017-year==age && (bread=="y" || insurance=="y")
-    result="Probably not a vampire."
+    result="Results inconclusive."
+
+    if 2017-year==age && (bread=="y" || insurance=="y")
+        result="Probably not a vampire."
+    end
+
+    if 2017-year!=age && (bread=="n" || insurance=="n")
+        result="Probably a vampire."
+    end
+
+    if 2017-year!=age && bread=="n" && insurance=="n"
+        result="Probably a vampire."
+    end
+
+    if 2017-year!=age && bread=="n" && insurance=="n"
+        result="Almost certainly a vampire."
+    end
+
+    if new_name=="Drake Cula" || new_name=="Tu Fang"
+        result="Definitely a vampire."
+    end
+
+    puts "#{result}"
+    i+=1
 end
-
-if 2017-year!=age && (bread=="n" || insurance=="n")
-    result="Probably a vampire."
-end
-
-if 2017-year!=age && bread=="n" && insurance=="n"
-    result="Probably a vampire."
-end
-
-if 2017-year!=age && bread=="n" && insurance=="n"
-    result="Almost certainly a vampire."
-end
-
-if new_name=="Drake Cula" || new_name=="Tu Fang"
-    result="Definitely a vampire."
-end
-
-puts "#{result}"
