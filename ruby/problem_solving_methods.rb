ac2f96138b6fe28c0097b22cd4ade75fe061a0aa
Erik Wotring <ewotring@gmail.com>
2017-05-16 20:14:01 -0500

    Add sequence for Release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,17 +1,35 @@
-arr = [46, 2, 98, 6]
-puts "provide a number"
-num_search=gets.to_i
+#arr = [46, 2, 98, 6]
+#puts "provide a number"
+#num_search=gets.to_i
+#
+#def search_array (x, num_search)
+#        x.each_with_index do |elements, index|
+#            if elements==num_search
+#                return index
+#            elsif index==x.length-1
+#                return "nil"
+#            else
+#                next
+#            end
+#        end
+#end
+#
+#puts search_array(arr, num_search)
 
-def search_array (x, num_search)
-        x.each_with_index do |elements, index|
-            if elements==num_search
-                return index
-            elsif index==x.length-1
-                return "nil"
-            else
-                next
-            end
-        end
+puts "length of sequence"
+sequence=gets.to_i
+#Build an array element by element.
+#Each element is the sum of the prior two elements.
+
+arr1=[0,1]
+i=2
+while i<sequence
+    arr1[i]=arr1[i-2]+arr1[i-1]
+    i+=1
 end
 
-puts search_array(arr, num_search)
+puts arr1
+
+if arr1[99]==218922995834555169026
+    puts "it matches!"
+end
