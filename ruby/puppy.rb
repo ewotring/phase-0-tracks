eaa9d4eca0240f76dd8b8b4a0d64fdec20373525
Erik Wotring <ewotring@gmail.com>
2017-05-23 21:41:23 -0500

    Add comments about Puppy class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,4 +1,15 @@
-SPECIES--------------
-Puppy
+#SPECIES ------------------------
+#Puppy Ofthehome
+#
+#CHARACTERISTICS ----------------
+#Height: varies
+#Friendly: yes
+#Leg count: 4
+#Fur color: varies
+#Eye color: varies
+#
+#BEHAVIOR -----------------------
+#Run
+#Cuddle
+#Nap
 
-CHARACTERISTICS------
