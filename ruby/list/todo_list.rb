54cfc67b174b27fc6b764fa9ef8b09e7ec0b1024
Erik Wotring <ewotring@gmail.com>
2017-05-27 15:14:47 -0500

    Add method to store given list

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,23 @@
+class TodoList
+    def initialize(list)
+        @list = list
+        
+    end
+
+    def get_items
+        get_list = @list
+    end
+
+    def add_item(item)
+        list<<item
+        list
+    end
+
+    def delete_item
+
+    end
+
+
+
+
+end
