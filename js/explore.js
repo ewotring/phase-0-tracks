45fd3abf330dae2cd4a8e86d9f7592fbcab0ac65
Erik Wotring <ewotring@gmail.com>
2017-06-01 16:55:05 -0500

    Start to pseudocode solution to reverse letters in a string

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// assign a string to a variable
+// split that string into an array
+// iterate through array in reverse
+// load each element into a new array
+// join that array back into a string
+
