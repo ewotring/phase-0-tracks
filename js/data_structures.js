27115411f9304bf95ed93a962851dc530e233fb6
Erik Wotring <ewotring@gmail.com>
2017-06-01 17:46:29 -0500

    Copy of code written by driver in pair session

---
+++ js/data_structures.js
@@ -0,0 +1,28 @@
+//Release 0 
+var color = ["purple", "iridescent blue", "black ash", "raspberry ombre"]
+var name =  ["Ed", "John", "Peter", "Alex"]
+var hash = {}
+color.push("white");
+name.push("Steve");
+console.log(color);
+console.log(name);
+//Release 1
+function Namecolors(name, color){
+ for(var i = 0; i < name.length; i++) {
+    hash[name[i]] = color[i];
+ }; 
+ return hash;
+ namecolors(name,color);
+console.log(hash);
+};
+//Release 2 
+function Car(name, numberofdoor, speed) {
+    console.log("our new car:", this);
+    this.sound = function() { console.log("Vroom!"); };
+    this.name = name;
+    this.numberofdoor = numberofdoor;
+    this.speed = speed;
+    console.log('your car is '  + this.name);
+};
+var car = new Car('lebarin', 2, 'fast'); 
+car.sound()
