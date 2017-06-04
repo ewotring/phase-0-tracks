8cd4d7bfe150d861beb524b55701a187df4a773e
Erik Wotring <ewotring@gmail.com>
2017-06-03 23:08:14 -0500

    Build string reversal function

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,24 @@
 // load each element into a new array
 // join that array back into a string
 
+var string = "";
+var splitString = []
+var reverseSplitString = []
+var reverseString = ""
+
+function reverseTheString(string) {
+    console.log(string)
+    splitString = string.split('');
+    console.log(splitString)
+     
+    for (var i = splitString.length-1; i >= 0; i -= 1) {
+        console.log(splitString[i]);
+        reverseSplitString.push(splitString[i]);
+    };
+    
+    console.log(reverseSplitString);
+    reverseString = reverseSplitString.join('');
+    console.log(reverseString)
+}
+
+reverseTheString("string")
