42db19fd77271a894c69b91d97cd4ea3b7f04d9f
Erik Wotring <ewotring@gmail.com>
2017-06-16 13:25:15 -0500

    Add /contact route

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,21 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# RELEASE 0
+
+# write a /contact route that displays an address (you can make up the address)
+
+get '/contact' do
+  "Address:<br>
+  5647 East Andrews St<br>
+  Chicago, IL<br>
+  60309"
+end
+
+
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+
+# A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+
+# Optional bonus: Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute. If you like, you can simply modify the home page to take a query parameter, and filter the students displayed if a query parameter is present.
