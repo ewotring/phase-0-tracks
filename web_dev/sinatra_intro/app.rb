7e78c75994217e2784af3653a2570611bf1618f0
Erik Wotring <ewotring@gmail.com>
2017-06-16 14:54:53 -0500

    Answer questions for Release 1:  Research on your Own

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -78,7 +78,7 @@ end
 
 # Optional bonus: Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute. If you like, you can simply modify the home page to take a query parameter, and filter the students displayed if a query parameter is present.
 
-# My pair and I did something like this in 9.5, before I got to this challenge, so I will base my attempt here on that code
+# My pair and I did something like this in 9.5, before I got to this challenge, so I will base my attempt here on that code and the above code to output the full student list.
 
 # students_campus had to be different from earlier students queries
 get '/students_campus/:campus' do
@@ -99,3 +99,15 @@ get '/students_campus/:campus' do
     response
   end
 end
+
+# Is Sinatra the only web app library in Ruby? What are some others?
+
+# It would seem that there are other web app libraries in Ruby. If frameworks fall under the set of libraries, Padrino and Rails are two other frameworks in Ruby.
+
+# Are SQLite and the sqlite3 gem your only options for using a database with Sinatra? What are some others?
+
+# There are other options for using databases with sinatra. One example is Datamapper. There are also sinatra extensions called ActiveRecord and Sequel.
+
+# What is meant by the term web stack?
+
+# The web stack is the group of tools needed for web development. Included in these tools are a database interface, a server, a programming language, and an operating system.
