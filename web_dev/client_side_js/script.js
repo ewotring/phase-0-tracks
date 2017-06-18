15cef2257ecf0895f820e099a9968ce990728c7a
Erik Wotring <ewotring@gmail.com>
2017-06-18 12:59:27 -0500

    Add modifications to the script file

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,11 @@
-console.log("I have performed in the theatre of the JavaScript.")
+console.log("I have performed in the theatre of the JavaScript.");
+
+function changeBorder(event){
+    console.log("The mouse moved over the element.");
+    console.log(event);
+    event.target.style.border = "thick double red";
+}
+
+var field = document.getElementById("play-with-js");
+field.addEventListener("mouseenter", changeBorder);
+// At first, I tried to use mouseover, but that added the thick double red border to all of the descendant elements when I moused over them. Changing to mouseenter took care of that issue. The difference, according to MDN, is that "it isn't sent when the pointer is moved from one of its descendants' physical space to its own physical space."
